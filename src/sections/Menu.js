475df88e29af519323f7ad033ce02bf2a4bddb2b
Dayne Wright <daynewr@gmail.com>
2018-12-27 16:42:59 -0600

    fixed colspan and class

--- src/sections/Menu.js
+++ src/sections/Menu.js
@@ -17,67 +17,67 @@ const Menu = () => (
     <Table className="pure-table pure-table-bordered">
       <thead>
         <tr>
-          <th colspan="2">Description</th>
+          <th colSpan="2">Description</th>
           <th>Price</th>
         </tr>
       </thead>
       <tbody>
         <tr className="pure-table-odd">
-          <td colspan="3"><b>STEAKS:</b></td>
+          <td colSpan="3"><b>STEAKS:</b></td>
         </tr>
         <tr>
-          <td colspan="2">Filet Mignon</td>
+          <td colSpan="2">Filet Mignon</td>
           <td>$16.39/lb</td>
         </tr>
         <tr>
-          <td colspan="2">Porterhouse</td>
+          <td colSpan="2">Porterhouse</td>
           <td>$10.89/lb</td>
         </tr>
         <tr>
-          <td colspan="2">T-Bone</td>
+          <td colSpan="2">T-Bone</td>
           <td>$10.69/lb</td>
         </tr>
         <tr>
-          <td colspan="2">Ribeye</td>
+          <td colSpan="2">Ribeye</td>
           <td>$12.08/lb</td>
         </tr>
         <tr className="pure-table-odd">
-        <td colspan="3"><b>DELI ITEMS:</b></td>
+        <td colSpan="3"><b>DELI ITEMS:</b></td>
         </tr>
         <tr>
-          <td colspan="2">Cajun Turkey Sandwich</td>
+          <td colSpan="2">Cajun Turkey Sandwich</td>
           <td>$4.09</td>
         </tr>
         <tr>
-          <td colspan="2">Mesquite Turkey Sandwich</td>
+          <td colSpan="2">Mesquite Turkey Sandwich</td>
           <td>$4.09</td>
         </tr>
         <tr>
-          <td colspan="2">BBQ Ham Sandwich</td>
+          <td colSpan="2">BBQ Ham Sandwich</td>
           <td>$4.09</td>
         </tr>
         <tr>
-          <td colspan="2">Chicken Sandwich</td>
+          <td colSpan="2">Chicken Sandwich</td>
           <td>$3.69</td>
         </tr>
         <tr>
-          <td colspan="2">Roast Beef Sandwich</td>
+          <td colSpan="2">Roast Beef Sandwich</td>
           <td>$4.89</td>
         </tr>
         <tr>
-          <td colspan="2">Ham Sandwich</td>
+          <td colSpan="2">Ham Sandwich</td>
           <td>$3.99</td>
         </tr>
         <tr>
-          <td colspan="2">Corn Beef Sandwich</td>
+          <td colSpan="2">Corn Beef Sandwich</td>
           <td>$5.29</td>
         </tr>
         <tr>
-          <td colspan="2">Reuben Sandwich</td>
+          <td colSpan="2">Reuben Sandwich</td>
           <td>$5.99</td>
         </tr>
         <tr>
-          <td colspan="2">Bologna Sandwich</td>
+          <td colSpan="2">Bologna Sandwich</td>
           <td>$3.09</td>
         </tr>
       </tbody>
